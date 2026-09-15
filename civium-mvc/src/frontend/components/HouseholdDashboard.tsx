8569cf723ba5361cf0eb81bfb91c8620d58ebf96
ChrisHG <235759192+chrishg23-jpg@users.noreply.github.com>
2026-09-15 15:18:58 +0100

    Add HouseholdDashboard component

---
+++ civium-mvc/src/frontend/components/HouseholdDashboard.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export function HouseholdDashboard() {
+  return (
+    <section style={{ marginBottom: '30px' }}>
+      <h2>Household Dashboard</h2>
+      <p>Your household information will appear here.</p>
+    </section>
+  );
+}
